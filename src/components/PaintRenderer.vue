6d043d8b48d3dd2bc8c3a530167e366d8357f2bb
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-09 21:11:56 +0200

    refactor: create component PaintRenderer

---
+++ src/components/PaintRenderer.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { computed, ref, unref, watchEffect, type MaybeRef, type Ref } from 'vue';
+import type { Crop, SaveParameters, Shape } from '../types'
+import { getArrowId } from '@/utils/getArrowId';
+import SvgShape from './SvgShape.vue';
+import { createDataUrl } from '@/utils/createDataUrl';
+
+const emit = defineEmits<{
+    (e: 'crop', crop: Crop | undefined): void
+    (e: 'save', { svg, crop }: SaveParameters): void
+    (e: 'clear'): void
+}>()
+
+const props = defineProps<{
+    /**
+     * Use background prop to set a background image. There are several ways to create a blob, like these:
+     * @example
+     * const blob1 = await canvasToBlob(canvas)
+     * const blob2 = await urlToBlob(url)
+     */
+    background?: MaybeRef<Blob>
+
+    activeShape: Shape | undefined
+    history: Shape[]
+    crop?: Crop
+    width: number
+    height: number
+}>()
+
+const allShapes = computed(() => props.activeShape ? [
+    props.activeShape,
+    ...props.history
+] : props.history)
+
+
+const backgroundSrc = ref()
+watchEffect(() => {
+    const unreffed = unref(props.background)
+    if (!unreffed) {
+        return undefined
+    }
+    createDataUrl(unreffed).then(src => backgroundSrc.value = src)
+})
+
+const arrowMarkers = computed(() =>
+    allShapes.value
+        .filter(shape => shape.type === 'arrow')
+        .filter((shape, index, self) => self.findIndex(s => getArrowId(s) === getArrowId(shape)) === index) // Unique matches only
+        .map(shape => ({
+            id: getArrowId(shape),
+            color: shape.color
+        }))
+)
+
+
+</script>
+
+<template>
+    <svg :width="width" :height="height" :viewBox="`0 0 ${width} ${height}`" xmlns="http://www.w3.org/2000/svg">
+        <image v-if="background" :xlink:href="backgroundSrc" :width="width" />
+        <svg-shape v-for="shape, i in history" :key="i" :shape="shape" />
+        <svg-shape v-if="activeShape" :shape="activeShape" />
+        <path v-if="crop" class="overlay" :d="`
+                M 0,0 V ${height} H ${width} V 0 Z
+                M ${crop.x},${crop.y} H ${crop.x + crop.width} V ${crop.y + crop.height} H ${crop.x} Z
+            `" />
+        <defs>
+            <marker v-for="marker in arrowMarkers" :id="marker.id" :key="marker.id" viewBox="0 0 10 10" refX="2.5"
+                refY="2.5" markerWidth="8" markerHeight="8" orient="auto-start-reverse">
+                <polygon points="0,5 1.7,2.5 0,0 5,2.5" :fill="marker.color"></polygon>
+            </marker>
+        </defs>
+        <svg:style>
+            rect,
+            line {
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            fill-opacity: 0;
+            }
+            .overlay {
+            opacity: 0.7;
+            fill: #020202;
+            }
+        </svg:style>
+    </svg>
+</template>
--- src/components/VueDraw.vue
+++ src/components/VueDraw.vue
@@ -2,8 +2,7 @@
 import { useElementBounding, usePointerSwipe } from '@vueuse/core'
 import { computed, ref, unref, watchEffect, type MaybeRef } from 'vue';
 import type { Crop, SaveParameters, Settings, Shape, Tool } from '../types'
-import { getArrowId } from '@/utils/getArrowId';
-import SvgShape from './SvgShape.vue';
+import PaintRenderer from './PaintRenderer.vue';
 import { createDataUrl } from '@/utils/createDataUrl';
 
 const emit = defineEmits<{
@@ -71,13 +70,7 @@ function updateActiveShape() {
     }
 }
 
-const allShapes = computed(() => activeShape.value ? [
-    activeShape.value,
-    ...history.value
-] : history.value)
-
-
-const { posStart, posEnd, distanceX, distanceY, isSwiping } = usePointerSwipe(svgRef, {
+const { posStart, posEnd } = usePointerSwipe(svgRef, {
     threshold: 0,
     onSwipe(e) {
         if (settings.value.tool === 'crop') {
@@ -131,7 +124,11 @@ function undo() {
 }
 
 function save() {
-    emit('save', { svg: svgRef, crop })
+    const svg = container.value.querySelector("svg")
+    if (!svg) {
+        throw new Error("Couldn't find the svg")
+    }
+    emit('save', { svg, crop })
 }
 
 function clear() {
@@ -140,49 +137,12 @@ function clear() {
     emit('clear')
 }
 
-const arrowMarkers = computed(() =>
-    allShapes.value
-        .filter(shape => shape.type === 'arrow')
-        .filter((shape, index, self) => self.findIndex(s => getArrowId(s) === getArrowId(shape)) === index) // Unique matches only
-        .map(shape => ({
-            id: getArrowId(shape),
-            color: shape.color
-        }))
-)
-
-
 </script>
 
 <template>
     <div ref="container" class="container">
-        <svg ref="svgRef" :width="width" :height="height" :viewBox="`0 0 ${width} ${height}`"
-            xmlns="http://www.w3.org/2000/svg">
-            <image v-if="background" :xlink:href="backgroundSrc" :width="width" />
-            <svg-shape v-for="shape, i in history" :key="i" :shape="shape" />
-            <svg-shape v-if="activeShape" :shape="activeShape" />
-            <path v-if="crop" class="overlay" :d="`
-                    M 0,0 V ${height} H ${width} V 0 Z
-                    M ${crop.x},${crop.y} H ${crop.x + crop.width} V ${crop.y + crop.height} H ${crop.x} Z
-                `" />
-            <defs>
-                <marker v-for="marker in arrowMarkers" :id="marker.id" :key="marker.id" viewBox="0 0 10 10" refX="2.5"
-                    refY="2.5" markerWidth="8" markerHeight="8" orient="auto-start-reverse">
-                    <polygon points="0,5 1.7,2.5 0,0 5,2.5" :fill="marker.color"></polygon>
-                </marker>
-            </defs>
-            <svg:style>
-                rect,
-                line {
-                stroke-linecap: round;
-                stroke-linejoin: round;
-                fill-opacity: 0;
-                }
-                .overlay {
-                opacity: 0.7;
-                fill: #020202;
-                }
-            </svg:style>
-        </svg>
+        <paint-renderer ref="svgRef" :activeShape :history :width :height :background :crop />
+
         <div class="toolbar">
             <button @click="setTool('crop')">Crop</button>
             <button @click="setTool('line')">Line</button>
@@ -202,7 +162,6 @@ const arrowMarkers = computed(() =>
     position: relative;
     cursor: crosshair;
 }
-
 
 .toolbar {
     position: absolute;
