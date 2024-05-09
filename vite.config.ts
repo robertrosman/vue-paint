211f1f1de42e09485bccf17928673197b8f793b9
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-09 06:56:53 +0200

    fix: register svg-prefixed elements as custom elements

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('svg:') 
+        }
+      }
+    }),
   ],
   resolve: {
     alias: {
