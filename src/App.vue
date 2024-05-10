d4937bf854e2d8d1152cb23cefd14c51afbbe0c2
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-10 19:48:23 +0200

    docs: add history example

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import PaintEditor from './components/PaintEditor.vue'
-import type { Settings, Crop, SaveParameters } from './types'
+import type { Settings, Crop, SaveParameters, Shape } from './types'
 import { toCanvas } from './utils/toCanvas';
 import { toImgSrc } from './utils/toImgSrc';
-import { useAsyncState } from '@vueuse/core';
+import { useAsyncState, useStorage } from '@vueuse/core';
 import { urlToBlob } from './utils/urlToBlob';
 
 const canvasRef = ref()
@@ -20,6 +20,15 @@ function save({ svg, crop }: SaveParameters) {
   toCanvas({ svg, canvas: canvasRef, crop })
 }
 
+const history = useStorage<Shape[]>("history", [
+  {
+    type: "crop",
+    x: 50,
+    y: 50,
+    width: 150,
+    height: 150
+  },
+])
 </script>
 
 <template>
@@ -38,4 +47,10 @@ function save({ svg, crop }: SaveParameters) {
 
   <h1>With background</h1>
   <paint-editor v-if="backgroundImage" class="vue-draw" @save="save" :background="backgroundImage"></paint-editor>
+
+  <h1>With history v-model</h1>
+  <p> Using <code>v-model:history</code> you can set initial state, modify the state programmatically, add shapes, save
+    current state (like in localStorage or on a server). Try to draw something and reload the page to see localStorage
+    in action. </p>
+  <paint-editor v-model:history="history" class="vue-draw" @save="save"></paint-editor>
 </template>
