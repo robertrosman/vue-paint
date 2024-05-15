53f5dd61c998a34ed614fb57d8ce1b2550a2573a
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-15 10:10:21 +0200

    chore: exclude public dir from package

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: false,
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
