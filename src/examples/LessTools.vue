24ee1aff31421d12653d091bbd4b6c6e2e0f7b33
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-20 16:10:14 +0200

    fix: remove dead code

--- src/examples/LessTools.vue
+++ src/examples/LessTools.vue
@@ -2,27 +2,10 @@
 import VpEditor from '@/components/VpEditor.vue'
 import { useRectangle } from '@/composables/tools/useRectangle'
 import { useArrow } from '@/composables/tools/useArrow'
-import type { Tool } from '@/types'
 
 const tools = [useRectangle(), useArrow()]
-
-type ExtractGeneric<Type> = Type extends Tool<infer S> ? S : never
-type ArrayElement<ArrayType extends readonly unknown[]> =
-  ArrayType extends readonly (infer ElementType)[] ? ElementType : never
-type History<Tools extends Array<unknown>> = ExtractGeneric<ArrayElement<Tools>>[]
-
-const arrow = useArrow()
-const onlyArrow = [arrow]
-const arrowHistory: History<typeof tools> = [
-  { type: 'arrow', x1: 0, x2: 0, y1: 0, y2: 0, thickness: 2, color: 'red' }
-]
-const firstArrow = arrowHistory[0]
-
-const shapes: History<typeof tools> = []
-
-shapes[0].type
-
 defineEmits(['save'])
+
 </script>
 
 <template>
