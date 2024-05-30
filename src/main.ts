2675c6093231451ca50a07511b163c2c31529674
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-30 07:13:10 +0200

    fix: export eraser and move

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ export { useCrop } from './composables/tools/useCrop/useCrop'
 export { useFreehand } from './composables/tools/useFreehand/useFreehand'
 export { useLine } from './composables/tools/useLine/useLine'
 export { useRectangle } from './composables/tools/useRectangle/useRectangle'
+export { useEraser } from './composables/tools/useEraser/useEraser'
+export { useMove } from './composables/tools/useMove/useMove'
 
 // Utils
 
@@ -20,6 +22,7 @@ export { createDataUrl } from './utils/createDataUrl'
 export { exportToCanvas } from './utils/exportToCanvas'
 export { urlToBlob } from './utils/urlToBlob'
 export { canvasToBlob } from './utils/canvasToBlob'
+export { randomId } from './utils/randomId'
 
 // Types
 export * from './types'
