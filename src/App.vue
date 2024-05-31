a615a5a49d1fb01d18e5363cde45aee91937436e
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-31 21:51:55 +0200

    refactor: remove obsolete imports

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import VpEditor from './components/VpEditor.vue'
 import type { SaveParameters } from './types'
 import { exportToCanvas } from './utils/export/exportToCanvas'
 import { exportSvg } from './utils/export/exportSvg'
-import { useAllTools } from '@/composables/tools/useAllTools'
 import InteractiveHistory from '@/examples/InteractiveHistory.vue'
 import PersistHistory from './examples/PersistHistory.vue'
 import UsingEvents from './examples/UsingEvents.vue'
@@ -13,8 +11,6 @@ import WithBackground from './examples/WithBackground.vue'
 
 const canvasRef = ref()
 const imgSrc = ref<string>()
-
-const { tools } = useAllTools()
 
 function save({ svg, tools, history }: SaveParameters) {
   imgSrc.value = exportSvg({ svg, tools, history })
