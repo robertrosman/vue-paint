c8ca6c6ca3e66629205782d3a500a08a7df3425a
Robert Rosman <robert.rosman@zmartagroup.com>
2024-05-31 21:51:39 +0200

    refactor: create separate downloadFile function

---
+++ src/utils/downloadFile.ts
@@ -0,0 +1,9 @@
+
+export function downloadFile(uri: string, filename: string) {
+  const link = document.createElement('a')
+  link.download = filename
+  link.href = uri
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
