763df87a810edddeebfc50abef2a1a9952e7d044
Robert Rosman <robert.rosman@zmartagroup.com>
2024-06-01 06:58:51 +0200

    refactor(demo): rename App.vue to Demo.vue

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import Demo from './Demo.vue'
 
-createApp(App).mount('#app')
+createApp(Demo).mount('#app')
