aa0c04f276bc4e2d41c42b2c3314c699b9925723
Robert Rosman <robert.rosman@zmartagroup.com>
2024-06-05 06:36:05 +0200

    test: mock Math.random instead of DI

--- src/utils/randomId.test.ts
+++ src/utils/randomId.test.ts
@@ -1,20 +1,25 @@
 import { randomId } from "@/utils/randomId";
-import { describe, expect, it } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
 
 
 describe('randomId', () => {
+    afterEach(() => {
+        vi.resetAllMocks()
+    })
     it('should return a string', () => {
         const id = randomId()
         expect(typeof id).toBe('string')
     })
 
     it('should return the lowest possible id (beginning with a letter)', () => {
-        const id = randomId(() => 0)
+        Math.random = vi.fn().mockReturnValue(0)
+        const id = randomId()
         expect(id).toBe('a000000')
     })
 
     it('should return the lowest possible id (beginning with a letter)', () => {
-        const id = randomId(() => 0.9999999999)
+        Math.random = vi.fn().mockReturnValue(0.9999999999)
+        const id = randomId()
         expect(id).toBe('zzzzzzz')
     })
 })
--- src/utils/randomId.ts
+++ src/utils/randomId.ts
@@ -2,6 +2,10 @@
  * Simple function to generate a random id. It is "unique" in the sense "the chance for collision is small enough", but not universally unique.
  * The generated id will start with a letter in order to be a valid css selector.
  */
-export function randomId(random = Math.random) {
-    return [26, 36, 36, 36, 36, 36, 36].map(x => Math.floor(random() * x + (36 - x)).toString(36)).join('')
+export function randomId() {
+    return [26, 36, 36, 36, 36, 36, 36].map(x => 
+        Math.floor(Math.random() * x + (36 - x))
+        .toString(36)
+    )
+    .join('')
 }
