0c00ec52d5d926e7cc722e3838bb912d161d83db
Robert Rosman <robert.rosman@zmartagroup.com>
2024-06-09 07:14:12 +0200

    feat: add edge handles to rectangle

--- src/composables/tools/useMove/handles/rectangleHandles.ts
+++ src/composables/tools/useMove/handles/rectangleHandles.ts
@@ -10,31 +10,51 @@ interface RectangleLike {
 export const rectangleHandles: Handle<RectangleLike>[] = [
     {
         name: 'base',
-        position: ({ x, y, width, height }: RectangleLike) => ({ x: x + width / 2, y: y + height / 2 }),
-        onMove: ({ x, y }: Movement) => ({ x, y })
+        position: ({ x, y, width, height }) => ({ x: x + width / 2, y: y + height / 2 }),
+        onMove: ({ x, y }) => ({ x, y })
     },
     {
-        name: 'top-left',
-        opposite: 'bottom-right',
-        position: ({ x, y }: RectangleLike) => ({ x, y }),
-        onMove: ({ x, y }: Movement) => ({ x, y, width: -x, height: -y })
+        name: 'top',
+        position: ({ x, y, width }) => ({ x: x + width / 2, y }),
+        onMove: ({ y }) => ({ y, height: -y })
     },
     {
         name: 'top-right',
         opposite: 'bottom-left',
-        position: ({ x, y, width }: RectangleLike) => ({ x: x + width, y }),
-        onMove: ({ x, y }: Movement) => ({ width: x, height: -y, y })
+        position: ({ x, y, width }) => ({ x: x + width, y }),
+        onMove: ({ x, y }) => ({ width: x, height: -y, y })
     },
     {
-        name: 'bottom-left',
-        opposite: 'top-right',
-        position: ({ x, y, height }: RectangleLike) => ({ x, y: y + height }),
-        onMove: ({ x, y }: Movement) => ({ x, width: -x, height: y })
+        name: 'right',
+        position: ({ x, y, width, height }) => ({ x: x + width, y: y + height / 2 }),
+        onMove: ({ x }) => ({ width: x })
     },
     {
         name: 'bottom-right',
         opposite: 'top-left',
-        position: ({ x, y, width, height }: RectangleLike) => ({ x: x + width, y: y + height }),
-        onMove: ({ x, y }: Movement) => ({ width: x, height: y })
-    }
+        position: ({ x, y, width, height }) => ({ x: x + width, y: y + height }),
+        onMove: ({ x, y }) => ({ width: x, height: y })
+    },
+    {
+        name: 'bottom',
+        position: ({ x, y, width, height }) => ({ x: x + width / 2, y: y + height }),
+        onMove: ({ y }) => ({ height: y })
+    },
+    {
+        name: 'bottom-left',
+        opposite: 'top-right',
+        position: ({ x, y, height }) => ({ x, y: y + height }),
+        onMove: ({ x, y }) => ({ x, width: -x, height: y })
+    },
+    {
+        name: 'left',
+        position: ({ x, y, height }) => ({ x, y: y + height / 2 }),
+        onMove: ({ x }) => ({ x, width: -x })
+    },
+    {
+        name: 'top-left',
+        opposite: 'bottom-right',
+        position: ({ x, y }) => ({ x, y }),
+        onMove: ({ x, y }) => ({ x, y, width: -x, height: -y })
+    },
 ]
